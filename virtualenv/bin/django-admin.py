388ca07058e049e80271f35839c36464a7ed7f05
fernandagonc <fernandarmg@gmail.com>
2021-08-03 18:51:42 -0300

    initial settings

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fernanda/Documentos/2021-1/teste soft/tdd-project/tdd-project/virtualenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
